ec082595baf82a0ddc27a86e3c830f0a1cab3b6c
Justin <jianhuatung@gmail.com>
2017-08-10 15:36:29 +0800

    添加Storyboard的跳转

--- StoryboardDemo/StoryboardDemo/ViewController.m
+++ StoryboardDemo/StoryboardDemo/ViewController.m
@@ -22,16 +22,17 @@
 
  // In a storyboard-based application, you will often want to do a little preparation before navigation
  - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
- // Get the new view controller using [segue destinationViewController].
- // Pass the selected object to the new view controller.
-     JHSecondViewController *vc = [segue destinationViewController];
      
-     vc.name = @"这是第二个VC";
-     
-      NSLog(@"FirstVC===========segue:%@  sender:%@",segue,sender);
+     if ([segue.identifier isEqualToString:@"SecondVC"]) {
+         JHSecondViewController *vc = [segue destinationViewController];
+         vc.name = @"这是第二个VC";
+         NSLog(@"FirstVC===========segue:%@  sender:%@",segue,sender);
+     } else {
+         NSLog(@"sender:%@",sender);
+     }
  }
 - (IBAction)jh_ThirdVC:(UIButton *)sender {
-    [self performSegueWithIdentifier:@"Third" sender:sender];
+    [self performSegueWithIdentifier:@"ThirdVC" sender:sender];
 }
 
 
