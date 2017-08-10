74810f5360655780c3504d3fa9efae93446f4778
Justin <jianhuatung@gmail.com>
2017-08-10 14:56:14 +0800

    添加StoryboardDemo

--- StoryboardDemo/StoryboardDemo/ViewController.m
+++ StoryboardDemo/StoryboardDemo/ViewController.m
@@ -21,15 +21,15 @@
 
 
  // In a storyboard-based application, you will often want to do a little preparation before navigation
-// - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-// // Get the new view controller using [segue destinationViewController].
-// // Pass the selected object to the new view controller.
-////     JHSecondViewController *vc = [segue destinationViewController];
-////     
-////     vc.name = @"这是第二个VC";
-////     
-////      NSLog(@"FirstVC===========segue:%@  sender:%@",segue,sender);
-// }
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+     JHSecondViewController *vc = [segue destinationViewController];
+     
+     vc.name = @"这是第二个VC";
+     
+      NSLog(@"FirstVC===========segue:%@  sender:%@",segue,sender);
+ }
 - (IBAction)jh_ThirdVC:(UIButton *)sender {
     [self performSegueWithIdentifier:@"Third" sender:sender];
 }
